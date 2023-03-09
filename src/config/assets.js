5312d5187883a63f72bb89f2d5cc5f6b42d02314
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-03-09 22:40:55 +0545

    [add]: assets

--- src/config/assets.js
+++ src/config/assets.js
@@ -4,4 +4,14 @@ export default [
   //   type: ['glbModel', 'basicTexture', 'videoTexture'],
   //   path: '',
   // },
+  {
+    name: 'boxModel',
+    type: 'glbModel',
+    path: 'models/box.glb',
+  },
+  {
+    name: 'boxTexture',
+    type: 'normalTexture',
+    path: 'textures/box_texture.png',
+  },
 ];
