c520234f336a181c2f22d5e21f6a8cf628b02c1e
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-08-03 21:04:14 +0545

    [update]: bin

--- bin/buildApp.js
+++ bin/buildApp.js
@@ -7,7 +7,7 @@ const { execSync } = require('child_process');
 if (process.argv.length < 3) {
   console.log('You have to provide a name to your app.');
   console.log('For example :');
-  console.log('    npx create-threejs-app my-app');
+  console.log('    npx build-threejs-app my-app');
   process.exit(1);
 }
 
